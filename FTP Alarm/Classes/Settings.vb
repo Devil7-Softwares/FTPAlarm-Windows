b525b16d3dd12c9994f5cb41742d3bde0d96d337
Devil7DK <dineshthangavel47@gmail.com>
2018-07-25 23:04:58 +0530

    Add Settings Object & Settings Manager

---
+++ FTP Alarm/Classes/Settings.vb
@@ -0,0 +1,42 @@
+﻿'==========================================================================
+'
+'                   (C) Copyright 2018 Devil7 Softwares.
+' 
+'      This file is part of 'Devil7 - College Management System' designed
+' for Government Arts College (Autonomous), Coimbatore, Tamilnadu, India.
+'
+'      Licensed under the Apache License, Version 2.0 (the "License");
+' you may Not use this file except in compliance with the License. You
+' may obtain a copy of the License at
+' 
+'           http://www.apache.org/licenses/LICENSE-2.0
+' 
+'      Unless required by applicable law Or agreed to in writing, software
+' distributed under the License Is distributed on an "AS IS" BASIS,
+' WITHOUT WARRANTIES Or CONDITIONS OF ANY KIND, either express Or implied.
+' See the License for the specific language governing permissions And
+' limitations under the License.
+' 
+' Contributors:
+'     Dineshkumar T
+'==========================================================================
+
+Public Class Settings
+
+    Property ServerAddress As String = "127.0.0.1"
+    Property Port As Integer = 21
+    Property Username As String = ""
+    Property Password As String = ""
+    Property IntervalHour As Integer = 0
+    Property IntervalMinutes As Integer = 10
+    Property RingtoneNotification As Boolean = True
+    Property VoiceNotification As Boolean = True
+    Property EmailNotification As Boolean = False
+    Property EmailFromAddress As String = "someone"
+    Property EmailToAddresses As String = "someone1@gmail.com;someone2@yahoo.com"
+    Property Voice As String = ""
+    Property Ringtone As String = "Default"
+    Property IncludeFiles As Boolean = False
+    Property MaxDepth As Integer = 2
+
+End Class
---
+++ FTP Alarm/Classes/SettingsManager.vb
@@ -0,0 +1,71 @@
+﻿'==========================================================================
+'
+'                   (C) Copyright 2018 Devil7 Softwares.
+' 
+'      This file is part of 'Devil7 - College Management System' designed
+' for Government Arts College (Autonomous), Coimbatore, Tamilnadu, India.
+'
+'      Licensed under the Apache License, Version 2.0 (the "License");
+' you may Not use this file except in compliance with the License. You
+' may obtain a copy of the License at
+' 
+'           http://www.apache.org/licenses/LICENSE-2.0
+' 
+'      Unless required by applicable law Or agreed to in writing, software
+' distributed under the License Is distributed on an "AS IS" BASIS,
+' WITHOUT WARRANTIES Or CONDITIONS OF ANY KIND, either express Or implied.
+' See the License for the specific language governing permissions And
+' limitations under the License.
+' 
+' Contributors:
+'     Dineshkumar T
+'==========================================================================
+
+Imports System.Xml.Serialization
+
+Module SettingsManager
+
+    Dim ConfigFile As String = IO.Path.Combine(My.Computer.FileSystem.SpecialDirectories.CurrentUserApplicationData, "Devil7", "FTP Alarm", "Settings.config")
+
+    Property Settings As New Settings
+
+    Sub LoadSettings()
+
+        Try
+            Dim S As New XmlSerializer(GetType(Settings))
+
+            If My.Computer.FileSystem.FileExists(ConfigFile) Then
+                Using FS As New IO.FileStream(ConfigFile, IO.FileMode.Open)
+                    Dim tmp = S.Deserialize(FS)
+                    If tmp IsNot Nothing Then
+                        Settings = tmp
+                    End If
+                End Using
+            End If
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+    Sub SaveSettings()
+
+        Try
+            Dim S As New XmlSerializer(GetType(Settings))
+
+            Dim FI As New IO.FileInfo(ConfigFile)
+
+            If Not My.Computer.FileSystem.DirectoryExists(FI.Directory.FullName) Then
+                FI.Directory.Create()
+            End If
+
+            Using FS As New IO.FileStream(ConfigFile, IO.FileMode.Create)
+                S.Serialize(FS, Settings)
+            End Using
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+End Module
